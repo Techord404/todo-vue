79351e930f119f15048de8b9e3fe81ca6904c7d4
Techord404 <an981022@naver.com>
2024-01-01 19:44:04 +0900

    Chapter 13

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form
-        @submit.prevent="onSubmit"
-        class="d-flex"
-    >
-      <div class="flex-grow-1 mr-2">
-        <input
-            class="form-control"
-            type="text"
-            v-model="todo"
-            placeholder="Type new to-do"
-        >
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+              class="form-control"
+              type="text"
+              v-model="todo"
+              placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button
+              class="btn btn-primary"
+              type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div>
-        <button
-            class="btn btn-primary"
-            type="submit"
-        >
-          Add
-        </button>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
       </div>
     </form>
     <div
@@ -40,6 +42,7 @@ import { ref } from "vue";
 
 export default {
   setup() {
+    const hasError = ref(false);
     const todo = ref('');
     const todos = ref([
       {id: 1, subject: '휴대폰 사기'},
@@ -47,16 +50,22 @@ export default {
     ]);
 
     const onSubmit = () => {
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value
-      });
-    }
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value
+        });
+        hasError.value = false;
+      }
+    };
 
     return {
       todo,
       todos,
       onSubmit,
+      hasError,
     };
   }
 }
